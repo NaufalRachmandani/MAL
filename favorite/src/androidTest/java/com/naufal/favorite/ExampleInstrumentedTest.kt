ce8e524705f316348833750fe43ed46f801d6989
naufalrachmandani <naufrach@gmail.com>
2022-01-30 15:07:55 +0700

    change directory expected in instrumented test

--- favorite/src/androidTest/java/com/naufal/favorite/ExampleInstrumentedTest.kt
+++ favorite/src/androidTest/java/com/naufal/favorite/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.naufal.favorite", appContext.packageName)
+        assertEquals("com.naufal.mal", appContext.packageName)
     }
 }
